318dc5717e6a6653906e5e632d9e1d8c725d13f8
Islon Scherer <islon@baltus.com>
2013-09-05 23:03:59 +0200

    removing unnecessary dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "http://github.com/stackoverflow/wally"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.2.6"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
